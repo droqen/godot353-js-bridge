2eb4e374a4651c6e5fed6151a95ed00da14c1923
pancelor <pancelor@gmail.com>
2024-03-31 23:35:10 -0700

    working! needed args

--- project/main.gd
+++ project/main.gd
@@ -1,19 +1,25 @@
 extends Control
 
-var window: JavaScriptObject
+onready var window = JavaScript.get_interface("window")
 onready var _henry_jso = JavaScript.create_callback(self,"_henry")
+onready var _george_jso = JavaScript.create_callback(self,"_george")
 
 func _on_MessageSend_pressed():
 	pass
 
 func _ready():
 	print("gd. _ready()")
-	window = JavaScript.get_interface("window")
-	print(window,_henry_jso)
+	window.button_cb = _george_jso
+	print(window,_henry_jso,window.button_cb)
 	window.gd_to_js(_henry_jso)
 
-func _henry():
-	var message = $m/v/MessageEdit.text
-	print("gd. Sending message '"+message+"'")
+# DO NOT FORGET ARGS HERE - SILENT FAILURE OTHERWISE
+func _henry(args):
+	print("gd. henry",args)
+	#var message = $m/v/MessageEdit.text
+	#print("gd. Sending message '"+message+"'")
 	#window.do_alert(message)
 
+func _george(args):
+	print("gd. george",args)
+	
