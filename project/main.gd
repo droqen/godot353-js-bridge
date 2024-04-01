5d45eb722e3c41e0a3307cf215762c4ebbb394b5
Alexander Martin <hi@droqen.com>
2024-04-01 01:41:42 -0400

    the button sends an alert! js!!!

--- project/main.gd
+++ project/main.gd
@@ -6,3 +6,6 @@ func _on_MessageSend_pressed():
 	var message = $m/v/MessageEdit.text
 	print("Sending message '"+message+"'")
 	JavaScript.eval("console.log('"+message+" (this is in JS)');")
+	var window = JavaScript.get_interface("window")
+	window.myFunc(message)
+	
